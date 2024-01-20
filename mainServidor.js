e3564cd1ee655dce562510f25d67ea5f1c79b189
David Valdaliso Lopez <dvaldaliso@gmail.com>
2024-01-20 02:13:06 +0100

    eliminado cosas innecesaria para esta entrega

--- mainServidor.js
+++ mainServidor.js
@@ -3,13 +3,6 @@ import ServerController from "./controller/serverController.js"
 function main() {
 	let tiempo = process.argv[3] !=null ? process.argv[3]: "12:00"
     let servidor =new ServerController("tcp://*", "5555", tiempo)
-  // Configurar la ejecución de la función cada 1000 milisegundos (1 segundo)
-  setInterval(function(){
-    if(servidor.volverAPedirHoras){
-        //servidor.dameHora()
-        console.log("dame hora de nuevo")
-    }
-  }, 30000);
    
 } 
 main() 
